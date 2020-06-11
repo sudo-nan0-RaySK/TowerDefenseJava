3ebf65e0cf5ef160c15563f189150ac6c766eec6
sudo-nan0-RaySK <saksham.sethi2017@vitstudent.ac.in>
2020-06-12 01:44:01 +0530

    feat: Added reader/parser for handling waves.txt

--- src/lists/WaveFileReader.java
+++ src/lists/WaveFileReader.java
@@ -1,5 +1,49 @@
 package lists;
 
-public class WaveFileReader {
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
 
+public class WaveFileReader {
+    public final String levelsFile = "res/levels/waves.txt";
+    public HashMap<Integer,ArrayList<String>> waveMap;
+    public boolean reachedEnd = false;
+    public int iterator = 0;
+
+    public WaveFileReader()throws  Exception{
+        this.waveMap = new HashMap<Integer, ArrayList<String>>();
+        File file  = new File(levelsFile);
+        Scanner sc = new Scanner(file);
+
+        while (sc.hasNext()){
+            String line = sc.nextLine();
+            String[] lineParams = line.trim().split(",");
+            if(waveMap.getOrDefault(Integer.parseInt(lineParams[0]),null)==null){
+                waveMap.put(Integer.parseInt(lineParams[0]),new ArrayList<String>());
+            }
+            waveMap.get(Integer.parseInt(lineParams[0])).add(line);
+        }
+    }
+
+    public ArrayList<String[]> next(){
+        ArrayList<String[]> ret = new ArrayList<>();
+        for(String dataLine : waveMap.get(iterator)){
+            ret.add(dataLine.split(","));
+        }
+        ++iterator;
+        if(waveMap.getOrDefault(iterator,null)==null){
+            reachedEnd = true;
+        }
+        return ret;
+    }
+
+    public boolean hasNext(){
+        if (reachedEnd){
+            return false;
+        }
+        return true;
+    }
 }
